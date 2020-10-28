cfa97a13a304e3b628ba1134e4f9e8102f86ee86
ooknimm <ooknimm@gmail.com>
2020-10-28 10:46:19 +0900

    pyc delete

--- test/test_view.py
+++ test/test_view.py
@@ -2,7 +2,7 @@ import pytest
 import bcrypt
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import config
 import json
 import io
